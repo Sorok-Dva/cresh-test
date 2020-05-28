85deabeeead056e3eeca5afdd8632f3ee660a8fc
Сорок два <garcia.llyam@gmail.com>
2020-05-28 23:49:17 +0200

    fix store_name validation

--- middlewares/HTTPValidation/Transaction.validation.js
+++ middlewares/HTTPValidation/Transaction.validation.js
@@ -11,7 +11,7 @@ const HTTPValidation = {};
  * @type {ValidationChain[]}
  */
 HTTPValidation.Create = [
-  check('store_name').exists().withMessage('Store Name cannot be empty.'),
+  check('store_name').exists().isString().withMessage('Store Name cannot be empty.'),
   check('amount').isNumeric().withMessage('Amount should be numeric.'),
   check('split').isNumeric().isInt({ gt: 1 }).withMessage('Split should be numeric and greater than 1.'),
   check('user_id').isNumeric().withMessage('User_id should be numeric.'),
