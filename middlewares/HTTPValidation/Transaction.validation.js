3145454082bfa0bf02c9f4714dfab7fa46547cf4
Сорок два <garcia.llyam@gmail.com>
2020-05-28 20:10:07 +0200

    add gt rule on split httpValidation

--- middlewares/HTTPValidation/Transaction.validation.js
+++ middlewares/HTTPValidation/Transaction.validation.js
@@ -13,7 +13,7 @@ const HTTPValidation = {};
 HTTPValidation.Create = [
   check('store_name').exists().withMessage('Store Name cannot be empty.'),
   check('amount').isNumeric().withMessage('Amount should be numeric.'),
-  check('split').isNumeric().withMessage('Split should be numeric.'),
+  check('split').isNumeric().isInt({ gt: 1 }).withMessage('Split should be numeric and greater than 1.'),
   check('user_id').isNumeric().withMessage('User_id should be numeric.'),
 ];
 
